7756c2df09f20360e7c526552fcec3a8e5a8a4f4
Vigneshvar Srinivasan <vigneshvar767@gmail.com>
2026-08-08 18:44:39 +0530

    Refactor DFS function for maze exploration

--- dfs_maze_treasure.py
+++ dfs_maze_treasure.py
@@ -1,35 +1,30 @@
 maze = {
-    "Entrance": ["Hallway", "Storage"],
-    "Hallway": ["Kitchen", "Living Room"],
-    "Kitchen": ["Dining Room"],
-    "Dining Room": ["Treasure Room"],
-    "Living Room": ["Bedroom"],
-    "Bedroom": ["Bathroom"],
-    "Bathroom": [],
-    "Storage": ["Garage"],
-    "Garage": ["Workshop"],
-    "Workshop": [],
-    "Treasure Room": []
+  "Entrance": ["Hallway", "Storage"],
+  "Hallway": ["Kitchen", "Living Room"],
+  "Kitchen": ["Dining Room"],
+  "Dining Room": ["Treasure Room"],
+  "Living Room": ["Bedroom"],
+  "Bedroom": ["Bathroom"],
+  "Bathroom": [],
+  "Storage": ["Garage"],
+  "Garage": ["Workshop"],
+  "Workshop": [],
+  "Treasure Room": []
 }
 
 visited = []
 
-
 def dfs(room):
-    if room not in visited:
-        print("Exploring:", room)
-        visited.append(room)
-
-        if room == "Treasure Room":
-            print("\nTreasure Found!")
-            return True
-
-        for next_room in maze[room]:
-            if dfs(next_room):
-                return True
-
-    return False
-
+  if room not in visited:
+    print("Exploring:", room)
+    visited.append(room)
+    if room == "Treasure Room":
+      print("\nTreasure Found!")
+      return True
+    for next_room in maze[room]:
+      if dfs(next_room):
+        return True
+  return False
 
 print("Robot starts searching...\n")
 dfs("Entrance")
